27f4056abb0ab8e5224e1dbfe365977d0d99f26d
gbavila-dev <gabriel13avila09@gmail.com>
2026-08-21 18:24:33 -0300

    Projeto cinema atualizado

--- sistema-cinema.py
+++ sistema-cinema.py
@@ -1,3 +1,11 @@
+cinema = []
+
+for i in range(5):
+    linha = []
+    for j in range(5):
+        linha.append("L")
+    cinema.append(linha)
+
 def menu():
     print("\n===================")
     print("SISTEMA - CINEMA")
@@ -5,91 +13,53 @@ def menu():
     print("1 - Mostrar cadeiras")
     print("2 - Reservar cadeira")
     print("3 - Cancelar reserva")
-    print("4 - Mostrar livres")
-    print("5 - Calcular resumo")
+    print("4 - Mostrar resumo")
     print("0 - Sair\n")   
 
-def mostrar_cadeiras(cadeiras):
-    print("- Cadeiras:")
-    for i in range(len(cadeiras)):
-        if cadeiras[i] == 0:
-            print(f"[{i+1}: L] ", end=" ")
-        else:
-            print(f"[{i+1}: O] ", end=" ")
-    
-    print()
+def mostrar_cadeiras(cinema):
+    for i in range(len(cinema)):
+        for j in range(len(cinema[i])):
+            print(f"[{i+1},{j+1}]: {cinema[i][j]}", end=" ")
+        print()
 
+def reservar_cadeira(cinema):
+    linha = int(input("Fileira: ")) - 1
+    coluna = int(input("Coluna: ")) - 1
 
-def reservar_cadeira(cadeiras):
-    try:
-        cadeira = int(input(f"- Insira o número da cadeira (1 a {len(cadeiras)}): "))
-        posicao = cadeira - 1
-
-        if posicao >= 0 and posicao < len(cadeiras):
-            if cadeiras[posicao] == 0:
-                cadeiras[posicao] = 1
-                print(f"Cadeira {cadeira} reservada com sucesso!")
-            else:
-                print("Essa cadeira já está reservada, selecione outra.")
-        else:
-            print("Número da cadeira inválido!")
-    except ValueError:
-        print("Insira apenas números!")
-
-def cancelar_reserva(cadeiras):
-    try:
-        cadeira = int(input("- Insira o número da cadeira (1 a 8): "))
-        posicao = cadeira - 1
-
-        if posicao >= 0 and posicao < len(cadeiras):
-            if cadeiras[posicao] == 1:
-                cadeiras[posicao] = 0
-                print(f"Reserva da cadeira {cadeira} cancelada com sucesso!")
-            else:
-                print("Essa cadeira não está ocupada.")
-        else:
-            print("Número da cadeira inválido!")
-    except ValueError:
-        print("Insira apenas números!")
-    
-def mostrar_livres(cadeiras):
-    contador = 0
-    
-    for i in range(len(cadeiras)):
-        if cadeiras[i] == 0:
-            contador += 1
-
-    print("- Cadeiras livres: ", end = " ")
-    for i in range(len(cadeiras)):
-        if cadeiras[i] == 0:
-            print(f"{i+1} ", end = " ")        
-    print()
-    if contador == 0:
-        print("Nenhuma cadeira está livre!")
+    if cinema[linha][coluna] == "L":
+        cinema[linha][coluna] = "O"
+        print("Reservado!")
     else:
-        print(f"{contador} cadeiras estão livres!")
+        print("Já está ocupado!")
 
+def cancelar_reserva(cinema):
+    linha = int(input("Fileira: ")) - 1
+    coluna = int(input("Coluna: ")) - 1
+
+    if cinema[linha][coluna] == "O":
+        cinema[linha][coluna] = "L"
+        print("Cancelado!")
+    else:
+        print("Essa cadeira já está livre!")
     
+def mostrar_resumo(cinema):
+    livres = 0
+    ocupadas = 0
 
-def calcular_resumo(cadeiras):
-    contador_l = 0
-    contador_r = 0
-
-    for i in range(len(cadeiras)):
-        if cadeiras[i] == 0:
-            contador_l += 1
-        else:
-            contador_r += 1
+    for i in range(len(cinema)):
+        for j in range(len(cinema[i])):
+            if cinema[i][j] == "L":
+                livres += 1
+            else:
+                ocupadas += 1
 
     print("- Resumo:")
     print()
-    print(f"Total de cadeiras: {len(cadeiras)}")
+    print(f"Total de cadeiras: {len(cinema) * len(cinema[0])}")
     print("-----------------------")
-    print(f"Livres: {contador_l} | Ocupadas: {contador_r}")
+    print(f"Livres: {livres} | Ocupadas: {ocupadas}")
     print()
 
-
-cadeiras = [0, 0, 0, 0, 0, 0, 0, 0]
 def main():
     while True:
         menu()
@@ -102,15 +72,13 @@ def main():
 
         match opcao:
             case 1:
-                mostrar_cadeiras(cadeiras)
+                mostrar_cadeiras(cinema)
             case 2:
-                reservar_cadeira(cadeiras)
+                reservar_cadeira(cinema)
             case 3:
-                cancelar_reserva(cadeiras)
+                cancelar_reserva(cinema)
             case 4: 
-                mostrar_livres(cadeiras)
-            case 5:
-                calcular_resumo(cadeiras)
+                mostrar_resumo(cinema)
             case 0:
                 print("Desligando o sistema...")
                 break
